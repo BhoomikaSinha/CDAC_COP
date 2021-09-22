826a58bab09c6b9a4fe02b2180994e15cd63e515
BhoomikaSinha <91183978+BhoomikaSinha@users.noreply.github.com>
2021-09-22 02:08:51 -0700

    Add files via upload

---
+++ 2addtion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("enter two values\n");
+    scanf("%d%d",&a,&b);
+    int c=(a+b);
+    printf("addition two number %d :",c);
+    return 0;
+}
