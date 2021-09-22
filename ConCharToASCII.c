596fb7d81db8607d8511928c834c589173ec715c
BhoomikaSinha <91183978+BhoomikaSinha@users.noreply.github.com>
2021-09-22 03:48:46 -0700

    Add files via upload

---
+++ ConCharToASCII.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    char a;
+    printf("enter character value\n");
+    scanf("%c",&a);
+    printf("character a convert ASCII number is %d :",a);
+    return 0;
+}
